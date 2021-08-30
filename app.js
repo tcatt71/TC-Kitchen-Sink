39a9f74ab8b30916bfbc6075c00de76acb333b6e
Troy Coffindaffer <troy.coffindaffer@gmail.com>
2021-08-30 10:58:16 -0700

    complete comment section of the exercise

--- app.js
+++ app.js
@@ -5,3 +5,10 @@ let isTrue = true;
 let resultOfAdding = 5 + 4;
 let compairingValues = 5 > 4;
 let multiplyingValues = 231 * 4;
+
+// Single line comment
+
+/* Multiline Comment
+let resultOfAdding = 5 + 4;
+let compairingValues = 5 > 4;
+let multiplyingValues = 231 * 4; */
