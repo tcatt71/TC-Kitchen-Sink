adcfd1e2cec76a41d947112a6cce774d73d739da
Troy Coffindaffer <troy.coffindaffer@gmail.com>
2021-08-30 18:40:39 -0700

    complete functions section of the exercise

--- app.js
+++ app.js
@@ -12,3 +12,20 @@ let multiplyingValues = 231 * 4;
 let resultOfAdding = 5 + 4;
 let compairingValues = 5 > 4;
 let multiplyingValues = 231 * 4; */
+
+function sayHello() {
+  alert('Hello World!');
+}
+
+sayHello();
+
+function checkAge(name, age) {
+  if (age < 21) {
+    alert("Sorry " + name + ", you aren't old enough to view this page!");
+  }
+}
+
+checkAge('Charles', 21);
+checkAge('Abby', 27);
+checkAge('James', 18);
+checkAge('John', 17);
